88baf4d5b717d3585d1feb619215544d8690eb05
V1k770r <testprueba066@gmail.com>
2023-12-15 18:56:28 -0500

    Configurando variables

--- src/main/java/com/AluraHotel/main/Main.java
+++ src/main/java/com/AluraHotel/main/Main.java
@@ -4,6 +4,10 @@ import com.AluraHotel.model.Huespedes;
 import com.AluraHotel.util.UtilEntity;
 
 import javax.persistence.EntityManager;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 public class Main {
@@ -13,6 +17,57 @@ public class Main {
         List<Huespedes> huespedes = em.createQuery("SELECT e FROM Huespedes e", Huespedes.class).getResultList();
         System.out.println("-------Listar TODOS---------");
         huespedes.forEach(System.out::println);
+
+        /*System.out.println("----CREAR UNO-------");
+
+        Huespedes newHuesped = new Huespedes();
+        newHuesped.setNombre("Karla");
+        newHuesped.setApellido("Perez");
+        newHuesped.setNacionalidad("Uruguaya");
+        newHuesped.setFechaDeNacimiento(new Date(1986, Calendar.FEBRUARY,10));
+        newHuesped.setTelefono("888888888");
+        newHuesped.setIdReserva(0002);
+
+        em.getTransaction().begin();
+        em.persist(newHuesped);
+        em.getTransaction().commit();
+
+        System.out.println("Nuevo empleado creado:" + newHuesped);*/
+
+        /*
+        System.out.println("-----BUSCAR UNO-----------");
+        int employeeId = 1;
+        Employee employee = em.find(Employee.class, employeeId);
+        System.out.println("Empleado encontrado" + employee);*/
+
+
+
+        /*System.out.println("----ACTUALIZAR------");
+        int employeeToUpdateId = 2;
+        Employee employeeToUpdate = em.find(Employee.class, employeeToUpdateId);
+        System.out.println("Empleado a modificar" + employeeToUpdate);
+
+        employeeToUpdate.setFirstName("Irving");
+        employeeToUpdate.setPaSurname("Juarez");
+        employeeToUpdate.setSalary(97000F);
+
+        em.getTransaction().begin();
+        em.merge(employeeToUpdate);
+        em.getTransaction().commit();
+
+        System.out.println("Empleado actualizado" + employeeToUpdate);*/
+
+        /*System.out.println("----ELIMINAR-----");
+        int employeeToDeleteId = 1;
+        Employee employeeToDelete = em.find(Employee.class, employeeToDeleteId);
+        System.out.println("Empleado a eliminar" + employeeToDelete);
+
+        em.getTransaction().begin();
+        em.remove(employeeToDelete);
+        em.getTransaction().commit();
+        em.close();*/
+
+
 
 
 
